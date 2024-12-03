84ff76eab9930e1376c183eeae38ee4b7205aaf1
Gongas <goncalo.mesteves03@gmail.com>
2024-12-03 21:02:41 +0000

    Added DQN setting

--- LunarLander/Configuration.py
+++ LunarLander/Configuration.py
@@ -5,7 +5,7 @@ CONFIG = {
     'MIN_FUEL': 5,                # Minimum Fuel Level of the SpaceCraft
     'MAX_FUEL': 10,               # Maximum Fuel Level of the SpaceCraft
     'N_ENVS': 4,                  # Number of Environments to consider when training
-    'N_ITERATIONS': 500_000,      # Number of Steps / Iterations to consider during Training
+    'N_ITERATIONS': 5_000_000,      # Number of Steps / Iterations to consider during Training
     'N_EPISODES': 10              # Number of Episodes to Consider
 }
 
@@ -17,7 +17,6 @@ PATHS_CONFIG = {
         },
         'DQN':{
             'Settings-1':'./ExperimentalResults/OriginalEnvironment/DQN/Settings-1/',
-            'Settings-2':'./ExperimentalResults/OriginalEnvironment/DQN/Settings-2/'
         },
     },
     'CustomEnvironment': {
@@ -27,7 +26,6 @@ PATHS_CONFIG = {
         },
         'DQN':{
             'Settings-1':'./ExperimentalResults/CustomEnvironment/DQN/Settings-1/',
-            'Settings-2':'./ExperimentalResults/CustomEnvironment/DQN/Settings-2/'
         },
     },
 }
@@ -42,4 +40,20 @@ PPO_SETTINGS_1 = {
     'ent_coef': 0.001,
     'learning_rate': get_linear_fn(start=2.5e-4, end=0, end_fraction=1),
     'clip_range': get_linear_fn(start=0.2, end=0, end_fraction=1),
+}
+
+# Define the Settings for the DQN Algorithm
+DQN_SETTING_1 = {
+    'n_timesteps': 1e5,
+    'learning_rate':  6.3e-4,
+    'batch_size': 128,
+    'buffer_size': 50000,
+    'learning_starts': 0,
+    'gamma': 0.99,
+    'target_update_interval': 250,
+    'train_freq': 4,
+    'gradient_steps': -1,
+    'exploration_fraction': 0.12,
+    'exploration_final_eps': 0.1,
+    'policy_kwargs': "dict(net_arch=[256, 256])",
 }
