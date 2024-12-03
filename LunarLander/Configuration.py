83cec6fed759b90d1946f5833a6a3d97e9c4ac01
Gongas <goncalo.mesteves03@gmail.com>
2024-12-03 14:42:46 +0000

    Configuration - Initial commit

---
+++ LunarLander/Configuration.py
@@ -0,0 +1,8 @@
+# Define a Global Dictionary to consider during the development of this Project
+CONFIG = {
+    'MIN_FUEL': 50,            # Minimum Fuel Level of the SpaceCraft
+    'MAX_FUEL': 100,           # Maximum Fuel Level of the SpaceCraft
+    'N_ENVS': 4,               # Number of Environments to consider when training
+    'N_ITERATIONS': 150_000,   # Number of Steps / Iterations to consider during Training
+    'N_EPISODES': 10           # Number of Episodes to Consider
+}
